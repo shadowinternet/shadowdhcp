8f30113b4325087bdaf5e3dc4f39e9f42b95aca1
Mackenzie Hauck <mackenzie@shadowinternet.ca>
2024-05-19 21:33:29 -0600

    modify test request to be wrapped in a RelayForw message

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,6 @@
 use dhcproto::{v6, Encodable};
 
-use std::net::UdpSocket;
+use std::net::{Ipv6Addr, UdpSocket};
 
 fn main() {
     let msg = dhcpv6_test_request().to_vec().expect("encoding test msg");
@@ -12,15 +12,26 @@ fn main() {
         .expect("couldn't send data");
 }
 
-#[allow(unused)]
-fn dhcpv6_test_request() -> v6::Message {
+fn dhcpv6_test_request() -> v6::RelayMessage {
     let duid = vec![
         29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
     ];
-    // construct a new Message with a random xid
+    // construct a new Solicit Message with a random xid
     let mut msg = v6::Message::new(v6::MessageType::Solicit);
     // set an option
     msg.opts_mut().insert(v6::DhcpOption::ClientId(duid));
 
-    msg
+    // package this message into a RelayForw Message
+    let mut relay_opts = v6::DhcpOptions::new();
+    relay_opts.insert(v6::DhcpOption::RelayMsg(v6::RelayMessageData::Message(msg)));
+
+    let mut relay_msg = v6::RelayMessage {
+        msg_type: v6::MessageType::RelayForw,
+        hop_count: 0,
+        link_addr: Ipv6Addr::new(8, 8, 8, 8, 8, 8, 8, 8),
+        peer_addr: Ipv6Addr::new(9, 9, 9, 9, 9, 9, 9, 9),
+        opts: relay_opts,
+    };
+
+    relay_msg
 }
